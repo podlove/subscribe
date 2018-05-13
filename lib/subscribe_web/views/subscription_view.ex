e3ab48aea2882eda72116c744eefb70271598602
Eric Teubert <eric@ericteubert.de>
2018-05-13 17:55:18 +0200

    pretty/minimal homepage URL

--- lib/subscribe_web/views/subscription_view.ex
+++ lib/subscribe_web/views/subscription_view.ex
@@ -4,4 +4,25 @@ defmodule SubscribeWeb.SubscriptionView do
   def render("config.json", %{button_opts: button_opts}) do
     button_opts
   end
+
+  @doc """
+  Render pretty URL without schema and trailing slash
+  """
+  def pretty_url(url) when is_binary(url) do
+    url
+    |> URI.parse()
+    |> pretty_url()
+  end
+
+  def pretty_url(%URI{host: host, path: "/"}) do
+    host
+  end
+
+  def pretty_url(%URI{host: host, path: nil}) do
+    host
+  end
+
+  def pretty_url(%URI{host: host, path: path}) do
+    host <> path
+  end
 end
