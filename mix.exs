15033bb2c3eca580eadd43bde36d725f10b3359d
Eric Teubert <eric@ericteubert.de>
2018-07-11 12:54:44 +0200

    update version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Subscribe.Mixfile do
   def project do
     [
       app: :subscribe,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
