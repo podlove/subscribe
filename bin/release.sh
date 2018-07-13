6f2c1b3e328f4124fcc53b52a7623aaf31991916
Eric Teubert <eric@ericteubert.de>
2018-07-13 15:44:51 +0200

    wip: url setup

--- bin/release.sh
+++ bin/release.sh
@@ -3,5 +3,5 @@
 set -e # exit on error
 
 mix edeliver build release production
-mix edeliver deploy release to production
+mix edeliver deploy release to production --version=0.0.2
 mix edeliver restart production
