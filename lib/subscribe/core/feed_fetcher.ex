220f13e419eebb04b3683052afd15048b790e679
Eric Teubert <eric@ericteubert.de>
2018-07-20 13:37:20 +0200

    FeedFetcher: catch ArgumentErrors

--- lib/subscribe/core/feed_fetcher.ex
+++ lib/subscribe/core/feed_fetcher.ex
@@ -1,17 +1,21 @@
 defmodule Subscribe.Core.FeedFetcher do
   def fetch(url) do
-    case HTTPoison.get(url, headers(), options()) do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body, headers: headers}} ->
-        {:ok, body, headers}
+    try do
+      case HTTPoison.get(url, headers(), options()) do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body, headers: headers}} ->
+          {:ok, body, headers}
 
-      {:ok, %HTTPoison.Response{status_code: 304}} ->
-        :notmodified
+        {:ok, %HTTPoison.Response{status_code: 304}} ->
+          :notmodified
 
-      {:ok, %HTTPoison.Response{status_code: 404}} ->
-        {:error, 404}
+        {:ok, %HTTPoison.Response{status_code: 404}} ->
+          {:error, 404}
 
-      {:error, %HTTPoison.Error{reason: reason}} ->
-        {:error, reason}
+        {:error, %HTTPoison.Error{reason: reason}} ->
+          {:error, reason}
+      end
+    rescue
+      ArgumentError -> {:error, :ArgumentError}
     end
   end
 
