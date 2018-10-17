993e5d95090d0a707bb023ce5e7f8455c36ce948
Eric Teubert <eric@ericteubert.de>
2018-10-17 20:06:47 +0200

    debug

--- bin/release.sh
+++ bin/release.sh
@@ -3,5 +3,5 @@
 set -e # exit on error
 
 mix edeliver build release production
-mix edeliver deploy release to production --version=0.0.2
+mix edeliver deploy release to production --version=0.1.0
 mix edeliver restart production
